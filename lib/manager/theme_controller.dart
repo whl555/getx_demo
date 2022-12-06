81ae18e109275456a710b59adca84c2bb114816a
whl <1677378469@qq.com>
2022-12-06 19:35:11 +0800

    feat: 增加夜间模式button变化

--- lib/manager/theme_controller.dart
+++ lib/manager/theme_controller.dart
@@ -6,13 +6,18 @@ class ThemeController extends GetxController{
   // UI状态——主题
   ThemeMode get theme => _loadTheme() ? ThemeMode.dark : ThemeMode.light;
 
+  final isDark = false.obs;
+
   // theme保存在GetStorage
   final _box = GetStorage();
   final _key = "isDarkMode";
 
   bool _loadTheme() => _box.read(_key) ?? false;
 
-  void saveTheme(bool isDarkMode) => _box.write(_key, isDarkMode);
+  void saveTheme(bool isDarkMode){
+    _box.write(_key, isDarkMode);
+    isDark(isDarkMode);
+  }
 
   void changeTheme(ThemeData themeData) => Get.changeTheme(themeData);
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,31 +8,26 @@ class HomePage extends StatelessWidget {
 
   final themeController = Get.find<ThemeController>();
 
-
   @override
   Widget build(BuildContext context) {
-
-
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("store"),
         actions: [
-          IconButton(
-              onPressed: (){
-                if(Get.isDarkMode){
+          Obx(() => IconButton(
+              onPressed: () {
+                if (Get.isDarkMode) {
                   themeController.saveTheme(false);
                   themeController.changeTheme(Themes.lightTheme);
                 } else {
                   themeController.saveTheme(true);
                   themeController.changeTheme(Themes.darkTheme);
                 }
-                print(Get.isDarkMode.toString());
+                print(themeController);
               },
-              icon: Get.isDarkMode
+              icon: themeController.isDark.value
                   ? const Icon(Icons.dark_mode_outlined)
-                  : const Icon(Icons.light_mode_outlined)
-          )
+                  : const Icon(Icons.light_mode_outlined)))
         ],
       ),
       body: Center(
