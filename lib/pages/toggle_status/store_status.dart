c0996fe9e4b009044258dbe2ae495cc0adffc699
whl <1677378469@qq.com>
2022-12-09 22:35:50 +0800

    feat: switch toggle update status

--- lib/pages/toggle_status/store_status.dart
+++ lib/pages/toggle_status/store_status.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:store/manager/store_controller.dart';
 
 class StoreStatus extends StatelessWidget {
-  const StoreStatus({Key? key}) : super(key: key);
+  StoreStatus({Key? key}) : super(key: key);
+
+  final storeController = Get.find<StoreController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("更新状态")),
-      body: Center(
-          child: Text("Update")
+      appBar: AppBar(title: const Text("更新状态")),
+      body: Container(
+        padding: const EdgeInsets.all(24),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Is the Store open?",
+                style: TextStyle(fontSize: 22),
+              ),
+              const SizedBox(height: 16),
+              Obx(
+                    () => Switch(
+                  onChanged: (value) => storeController.isStoreOpen(value),
+                  activeColor: Colors.green,
+                  value: storeController.isStoreOpen.value,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
